ebf68faddba44549e81b4e39b285821ed3fe0b3a
Matthew Collins <matthew.collins@defra.gov.uk>
2024-09-12 15:48:14 +0100

    Add performance tracking to external calls

--- server/util.js
+++ server/util.js
@@ -1,4 +1,5 @@
 const { HttpsProxyAgent } = require('https-proxy-agent')
+const { performance } = require('node:perf_hooks')
 
 const config = require('./config')
 const wreck = require('@hapi/wreck').defaults({
@@ -14,23 +15,37 @@ if (config.http_proxy) {
 }
 function get (url, options, ext = false) {
   const thisWreck = (ext && wreckExt) ? wreckExt : wreck
+  const startTick = performance.now()
   return thisWreck.get(url, options)
     .then(response => {
+      console.log('Response received from url in %d ms : %s', performance.now() - startTick, url)
       if (response.res.statusCode !== 200) {
         throw new Error('Requested resource returned a non 200 status code', response)
       }
       return response.payload
     })
+    .catch(error => {
+      console.log('Error received from url in %d ms : %s', performance.now() - startTick, url)
+      console.log(error)
+      throw error
+    })
 }
 
 function post (url, options, ext = false) {
   const thisWreck = (ext && wreckExt) ? wreckExt : wreck
+  const startTick = performance.now()
   return thisWreck.post(url, options)
     .then(response => {
+      console.log('Response received from url in %d ms : %s', performance.now() - startTick, url)
       if (response.res.statusCode !== 200) {
         throw new Error('Requested resource returned a non 200 status code', response)
       }
       return response.payload
+    })
+    .catch(error => {
+      console.log('Error received from url in %d ms : %s', performance.now() - startTick, url)
+      console.log(error)
+      throw error
     })
 }
 
