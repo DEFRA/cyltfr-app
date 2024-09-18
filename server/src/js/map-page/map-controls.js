1d78c925e7994049f7be24a1ca1cfecb09b9d1aa
michael steinacher <steinacherm@hotmail.co.uk>
2024-09-18 13:57:22 +0100

    Fixed position of close button on devices and removed currently unused function for radio  selection.

--- server/src/js/map-page/map-controls.js
+++ server/src/js/map-page/map-controls.js
@@ -1,15 +1,5 @@
 import { mapControlsConsts } from './constants'
 
-export function selectedOption () {
-  // The below const cannot be removed from the file otherwise it breaks reservoirs and rivers and the sea
-  const measurementsRadios = document.querySelector('input[name="measurements"]:checked')
-
-  if (mapControlsConsts.extentRadioSw.checked) {
-    return measurementsRadios.value
-  }
-  return measurementsRadios.value
-}
-
 export function openKey () {
   mapControlsConsts.keyDisplay.classList.remove('hide')
   mapControlsConsts.openKeyBtn.classList.add('hide')
