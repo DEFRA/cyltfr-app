60f4a1115215f13da4eb4dc5ae38e603af2e842b
Michael Steinacher <49474728+MickStein@users.noreply.github.com>
2024-09-19 09:28:03 +0100

    Dev build: basic rivers and sea map (#28)
    * Dev build: basic rivers and sea map
    
    https://eaflood.atlassian.net/browse/LTFRI-1412
    
    Implementing the new rivers and sea map page.

--- server/src/js/map-page/map-controls.js
+++ server/src/js/map-page/map-controls.js
@@ -3,10 +3,21 @@ import { mapControlsConsts } from './constants'
 export function selectedOption () {
   // The below const cannot be removed from the file otherwise it breaks reservoirs and rivers and the sea
   const measurementsRadios = document.querySelector('input[name="measurements"]:checked')
+  const currentPageURL = new URLSearchParams(document.location.search)
+  const mapPageQuery = currentPageURL.get('map')
+  const currentPageSurfaceWater = mapPageQuery === 'SurfaceWater'
 
-  if (mapControlsConsts.extentRadioSw.checked) {
-    return measurementsRadios.value
+  // The below is a placeholder ready to add the radio changes for climate change data
+  if (currentPageSurfaceWater) {
+    if (mapControlsConsts.extentRadioSw.checked) {
+      return measurementsRadios.value
+    }
+  } else {
+    if (mapControlsConsts.extentRadioRs.checked) {
+      return measurementsRadios.value
+    }
   }
+
   return measurementsRadios.value
 }
 
