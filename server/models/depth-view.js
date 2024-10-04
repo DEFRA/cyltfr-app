bc50a78cab397621ac7a7b9b0bee4e5ab6fb3000
Matthew Collins <matthew.collins@defra.gov.uk>
2024-10-04 09:39:17 +0100

    New depth page for rivers and sea

---
+++ server/models/depth-view.js
@@ -0,0 +1,23 @@
+const RiskLevel = {
+  VeryLow: 'Very Low',
+  Low: 'Low',
+  Medium: 'Medium',
+  High: 'High'
+}
+
+const RiskTitles = {
+  'Very Low': 'Very low risk',
+  Low: 'Low risk',
+  Medium: 'Medium risk',
+  High: 'High risk'
+}
+
+function depthViewModel (depthJson) {
+  const depthData = {}
+
+
+
+  return depthData
+}
+
+module.exports = depthViewModel
--- server/services/risk.js
+++ server/services/risk.js
@@ -2,6 +2,8 @@ const util = require('../util')
 const config = require('../config')
 const url = config.serviceUrl
 const floodRiskUrl = url + '/floodrisk/'
+const swDepthRiskUrl = url + '/swdepth/'
+const rsDepthRiskUrl = url + '/rsdepth/'
 
 function getByCoordinates (x, y, radius) {
   const uri = floodRiskUrl + x + '/' + y + '/' + radius
@@ -9,6 +11,18 @@ function getByCoordinates (x, y, radius) {
   return util.getJson(uri)
 }
 
+function swDepthRisk (x, y) {
+  const uri = swDepthRiskUrl + x + '/' + y
+
+  return util.getJson(uri)
+}
+
+function rsDepthRisk (x, y) {
+  const uri = rsDepthRiskUrl + x + '/' + y
+
+  return util.getJson(uri)
+}
+
 module.exports = {
-  getByCoordinates
+  getByCoordinates, swDepthRisk, rsDepthRisk
 }
