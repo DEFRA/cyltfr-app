4eb630888ab58d835fc20ec544b0c803c1306bf3
Matthew Collins <matthew.collins@defra.gov.uk>
2024-11-05 11:15:27 +0000

    Update util to make performance test optional

--- server/util.js
+++ server/util.js
@@ -1,11 +1,12 @@
 const { HttpsProxyAgent } = require('https-proxy-agent')
-const { performance } = require('node:perf_hooks')
 
 const config = require('./config')
 const wreck = require('@hapi/wreck').defaults({
   timeout: config.httpTimeoutMs
 })
 let wreckExt
+let get
+let post
 
 if (config.http_proxy) {
   wreckExt = require('@hapi/wreck').defaults({
@@ -13,40 +14,65 @@ if (config.http_proxy) {
     agent: new HttpsProxyAgent(config.http_proxy)
   })
 }
-function get (url, options, ext = false) {
-  const thisWreck = (ext && wreckExt) ? wreckExt : wreck
-  const startTick = performance.now()
-  return thisWreck.get(url, options)
-    .then(response => {
-      console.log('Response received from url in %d ms : %s', performance.now() - startTick, url)
-      if (response.res.statusCode !== 200) {
-        throw new Error('Requested resource returned a non 200 status code', response)
-      }
-      return response.payload
-    })
-    .catch(error => {
-      console.log('Error received from url in %d ms : %s', performance.now() - startTick, url)
-      console.log(error)
-      throw error
-    })
-}
 
-function post (url, options, ext = false) {
-  const thisWreck = (ext && wreckExt) ? wreckExt : wreck
-  const startTick = performance.now()
-  return thisWreck.post(url, options)
-    .then(response => {
-      console.log('Response received from url in %d ms : %s', performance.now() - startTick, url)
-      if (response.res.statusCode !== 200) {
-        throw new Error('Requested resource returned a non 200 status code', response)
-      }
-      return response.payload
-    })
-    .catch(error => {
-      console.log('Error received from url in %d ms : %s', performance.now() - startTick, url)
-      console.log(error)
-      throw error
-    })
+if (config.performanceLogging) {
+  const { performance } = require('node:perf_hooks')
+  get = (url, options, ext = false) => {
+    const thisWreck = (ext && wreckExt) ? wreckExt : wreck
+    const startTick = performance.now()
+    return thisWreck.get(url, options)
+      .then(response => {
+        console.log('Response received from url in %d ms : %s', performance.now() - startTick, url)
+        if (response.res.statusCode !== 200) {
+          throw new Error('Requested resource returned a non 200 status code', response)
+        }
+        return response.payload
+      })
+      .catch(error => {
+        console.log('Error received from url in %d ms : %s', performance.now() - startTick, url)
+        console.log(error)
+        throw error
+      })
+  }
+
+  post = (url, options, ext = false) => {
+    const thisWreck = (ext && wreckExt) ? wreckExt : wreck
+    const startTick = performance.now()
+    return thisWreck.post(url, options)
+      .then(response => {
+        console.log('Response received from url in %d ms : %s', performance.now() - startTick, url)
+        if (response.res.statusCode !== 200) {
+          throw new Error('Requested resource returned a non 200 status code', response)
+        }
+        return response.payload
+      })
+      .catch(error => {
+        console.log('Error received from url in %d ms : %s', performance.now() - startTick, url)
+        console.log(error)
+        throw error
+      })
+  }
+} else {
+  get = (url, options, ext = false) => {
+    const thisWreck = (ext && wreckExt) ? wreckExt : wreck
+    return thisWreck.get(url, options)
+      .then(response => {
+        if (response.res.statusCode !== 200) {
+          throw new Error('Requested resource returned a non 200 status code', response)
+        }
+        return response.payload
+      })
+  }
+  post = (url, options, ext = false) => {
+    const thisWreck = (ext && wreckExt) ? wreckExt : wreck
+    return thisWreck.post(url, options)
+      .then(response => {
+        if (response.res.statusCode !== 200) {
+          throw new Error('Requested resource returned a non 200 status code', response)
+        }
+        return response.payload
+      })
+  }
 }
 
 function getJson (url, ext = false) {
