7ce431942af5802982dd9ed4b32dbd293b1d45c8
Matthew Collins <matthew.collins@defra.gov.uk>
2024-11-14 10:39:22 +0000

    Remove debug code

--- server/services/address.js
+++ server/services/address.js
@@ -11,7 +11,6 @@ async function simulatedFind (inputPostcode) {
     const postcode = inputPostcode.toUpperCase()
     const part1 = postcode.split(' ')[0]
     const filename = path.join(config.simulatedDataPath, part1, postcode + '.json')
-    console.log(filename)
     const filedata = JSON.parse(await fs.readFile(filename))
     output = filedata.results.map(item => item.DPA ? item.DPA : item.LPI).map(item => {
       return {
