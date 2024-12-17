bc4b1523da1e4580e1954921169dbd720c69eb7d
Michael Steinacher <49474728+MickStein@users.noreply.github.com>
2024-12-17 11:28:59 +0000

    All risks on depth pages appear blue in colour (#90)
    * All risks on depth pages appear blue in colour
    
    https://eaflood.atlassian.net/browse/LTFRI-1633
    
    Task - change colour scheme to match the ‘what the flood risk ratings mean' guidance.

--- server/models/__tests__/depth-view.spec.js
+++ server/models/__tests__/depth-view.spec.js
@@ -31,24 +31,24 @@ describe('Depth view model', () => {
     expect(result).toMatchObject({ rs200: 'Medium', rs200cc: 'Very low' })
   })
 
-  test('Sets Surface water class name correctly', async () => {
+  test('Sets Surface water class name correctly in lower case', async () => {
     const swDepth = {
       200: {
         current: 'Medium'
       }
     }
     const result = depth(swDepth, null, address)
-    expect(result).toMatchObject({ sw200: 'Medium', sw200cc: 'Very low', sw200ccclassname: 'Very-low' })
+    expect(result).toMatchObject({ sw200: 'Medium', sw200cc: 'Very low', sw200ccclassname: 'very-low' })
   })
 
-  test('Sets Rivers and sea class name correctly', async () => {
+  test('Sets Rivers and sea class name correctly in lower case', async () => {
     const rsDepth = {
       200: {
         current: 'Medium'
       }
     }
     const result = depth(null, rsDepth, address)
-    expect(result).toMatchObject({ rs200: 'Medium', rs200cc: 'Very low', rs200ccclassname: 'Very-low' })
+    expect(result).toMatchObject({ rs200: 'Medium', rs200cc: 'Very low', rs200ccclassname: 'very-low' })
   })
 
   test('Sets Rivers and sea level change', async () => {
