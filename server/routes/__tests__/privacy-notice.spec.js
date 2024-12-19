6a5f4e2a6d530fe5687c19b48255d61a7bf83d6c
Joshua Forrest <162310725+JoshuaForrest@users.noreply.github.com>
2024-12-19 14:48:03 +0000

    Headings hierarchy (Rivers and sea and surface water depth) (#95)
    https://eaflood.atlassian.net/jira/software/c/projects/LTFRI/boards/589?selectedIssue=LTFRI-1661
    
    This change fixes issue with heading hierarchy across different pages for accessability.

--- server/routes/__tests__/privacy-notice.spec.js
+++ server/routes/__tests__/privacy-notice.spec.js
@@ -24,7 +24,7 @@ describe('/Privacy notice test', () => {
     const response = await server.inject(options)
     expect(response.statusCode).toEqual(STATUS_CODES.HTTP_STATUS_OK) // 200
     expect(response.payload).toMatch(/<h1 class="govuk-heading-l">Privacy notice<\/h1>/g)
-    expect(response.payload).toMatch(/<h1 class="govuk-heading-m">Who we are<\/h1>/g)
+    expect(response.payload).toMatch(/<h2 class="govuk-heading-m">Who we are<\/h2>/g)
     expect(response.payload).toMatch(/<h2 class="govuk-heading-m">What personal data we collect<\/h2>/g)
     expect(response.payload).toMatch(/<h3 class="govuk-heading-s">Cookies<\/h3>/g)
     expect(response.payload).toMatch(/<h3 class="govuk-heading-s">Friendly Captcha<\/h3>/g)
