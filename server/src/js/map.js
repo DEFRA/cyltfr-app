f6db1abb60da49b894fbe48bee48ccf87f38f96b
pmshaw15 <paul.shaw@defra.gov.uk>
2025-01-02 13:25:20 +0000

    Swap Ordnance Survey basemap for ArcGIS Online

--- server/src/js/map.js
+++ server/src/js/map.js
@@ -1,39 +1,17 @@
-import Extent from '@arcgis/core/geometry/Extent.js'
 import ArcMap from '@arcgis/core/Map.js'
 import Basemap from '@arcgis/core/Basemap.js'
 import MapView from '@arcgis/core/views/MapView.js'
 import Point from '@arcgis/core/geometry/Point.js'
 import SpatialReference from '@arcgis/core/geometry/SpatialReference.js'
 import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer.js'
-import WebTileLayer from '@arcgis/core/layers/WebTileLayer.js'
-import esriConfig from '@arcgis/core/config.js'
 import Graphic from '@arcgis/core/Graphic.js'
 
-let map, callback, currentLayer, tokenFetchRunning
-const TOKEN_PREFETCH_SECS = 30
+let map, callback, currentLayer
 const currentPageURL = new URLSearchParams(document.location.search)
 const mapPageQuery = currentPageURL.get('map')
-const initialZoomLevel = mapPageQuery === null ? 1 : 7
-
-async function refreshOsToken () {
-  tokenFetchRunning = true
-  const response = await fetch('/os-get-token')
-  tokenFetchRunning = false
-  if (response.ok) {
-    const tokenValues = await response.json()
-    window.mapConfig.osToken = tokenValues.access_token
-    setTimeout(() => {
-      refreshOsToken()
-    }, (tokenValues.expires_in - TOKEN_PREFETCH_SECS) * 1000)
-  } else {
-    throw new Error('os-get-token failed')
-  }
-}
+const initialZoomLevel = mapPageQuery === null ? 6 : 13
 
 export async function loadMap (point) {
-  // Add the base layer map
-
-  const { layer } = createBaseLayer()
   const layers = []
 
   // Create the vector layers
@@ -41,7 +19,9 @@ export async function loadMap (point) {
 
   map = new ArcMap({
     basemap: new Basemap({
-      baseLayers: [layer]
+      portalItem: {
+        id: 'a8c045aa74d643cc9e2fa2702cc4cb45'
+      }
     }),
     layers
   })
@@ -60,10 +40,10 @@ export async function loadMap (point) {
     zoom: initialZoomLevel,
     center: centrePoint,
     constraints: {
-      minZoom: 0,
-      maxZoom: 9,
+      minZoom: 6,
       rotationEnabled: false
-    }
+    },
+    spatialReference: new SpatialReference({ wkid: 27700 })
   })
 
   const markerPoint = new Point({
@@ -125,71 +105,10 @@ function createFeatureLayers (layers) {
   }
 }
 
-function createBaseLayer () {
-  const bng = new SpatialReference({ wkid: 27700 })
-
-  const extent = new Extent({
-    xmin: -238375.0,
-    ymin: 0.0,
-    xmax: 900000.0,
-    ymax: 1376256.0,
-    spatialReference: bng
-  })
-
-  const layer = new WebTileLayer({
-    id: 'base-map',
-    urlTemplate: window.mapConfig.osMapUrl + '{level}/{col}/{row}.png',
-    fullExtent: extent,
-    spatialReference: bng,
-    tileInfo: {
-      lods: [
-        { level: 0, resolution: 896.0, scale: 3386450 },
-        { level: 1, resolution: 448.0, scale: 1693225 },
-        { level: 2, resolution: 224.0, scale: 846612 },
-        { level: 3, resolution: 112.0, scale: 423306 },
-        { level: 4, resolution: 56.0, scale: 211653 },
-        { level: 5, resolution: 28.0, scale: 105827 },
-        { level: 6, resolution: 14.0, scale: 52913 },
-        { level: 7, resolution: 7.0, scale: 26457 },
-        { level: 8, resolution: 3.5, scale: 13228 },
-        { level: 9, resolution: 1.75, scale: 6614 }
-      ],
-      origin: new Point({
-        x: -238375.0,
-        y: 1376256.0,
-        spatialReference: bng
-      }),
-      spatialReference: bng
-    }
-  })
-
-  setTimeout(() => { refreshOsToken() }, (window.mapConfig.osTokenExpiry - TOKEN_PREFETCH_SECS) * 1000)
-
-  esriConfig.request.interceptors.push({
-    urls: window.mapConfig.osMapHost,
-    headers: { Authorization: `Bearer ${window.mapConfig.osToken}` },
-    before: (params) => {
-      if (params.requestOptions.headers) {
-        params.requestOptions.headers.Authorization = `Bearer ${window.mapConfig.osToken}`
-      }
-    },
-    error: (e) => {
-      if ((e.name === 'request:server') && (!tokenFetchRunning)) {
-        tokenFetchRunning = setTimeout(async () => {
-          await refreshOsToken()
-          layer.refresh()
-        }, 0)
-      }
-    }
-  })
-
-  return { layer }
-}
-
 export function showMap (layerReference, hasLocation) {
   map.allLayers.forEach(function (layer) {
     const layerName = layer.id
-    if (layerName !== 'base-map') {
+    if (layer.title !== 'GB Base OS Outdoors Style') {
       currentLayer = layerName === layerReference ? layer : currentLayer
       if (layerName === layerReference) {
         layer.visible = true
