13eb0074872812bcfb730e90928706d5ac07e6b3
JoshuaForrest <josh.forrest@aol.com>
2025-01-13 12:19:00 +0000

    Added backlink to terms and conditions page

--- server/services/defineBackLink.js
+++ server/services/defineBackLink.js
@@ -1,5 +1,10 @@
 function defineBackLink (currentPage, previousPage) {
-  if (currentPage === '/map' || currentPage === '/cookies' || currentPage === '/privacy-notice') {
+  if (
+    currentPage === '/map' ||
+    currentPage === '/cookies' ||
+    currentPage === '/privacy-notice' ||
+    currentPage === '/terms-and-conditions'
+  ) {
     return previousPage || '/postcode'
   }
   return '/postcode'
