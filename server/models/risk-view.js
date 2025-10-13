16d949b3524ffee3d6e00d6b4e46c11871c200bd
Joshua Forrest <162310725+JoshuaForrest@users.noreply.github.com>
2025-10-13 09:29:44 +0100

    Investigate potential logic issue with sign up for flood warnings link (#183)
    https://eaflood.atlassian.net/browse/LTFRI-2092
    
    This update will change the method the sign up for flood warnings link will show by using the flood alert area data instead.

--- server/models/risk-view.js
+++ server/models/risk-view.js
@@ -68,6 +68,9 @@ function riskViewModel (risk, address, backLinkUri) {
   this.hasHoldingComments = false
   this.hasLlfaComments = false
 
+  // Flood Warning area
+  this.isFloodWarningArea = Array.isArray(risk.floodWarningAreas) && risk.floodWarningAreas.length > 0
+
   // Extra info
   processExtraInfo.call(this, risk)
 
