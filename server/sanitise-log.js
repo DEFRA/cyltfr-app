ab593839a7269f4bdb3bf4d065fa4295ac667fe0
MJQ7 <matthew.quinton@defra.gov.uk>
2025-11-27 12:53:29 +0000

    Improve readability and duplicate calls to config
    UPDATED: only check if the local env flag is set once
    
    Changes:
    	sanitise-log.js updated

--- server/sanitise-log.js
+++ server/sanitise-log.js
@@ -1,10 +1,14 @@
 module.exports = function logSanitisedConfig (config, protectedProperties) {
-  const sanitisedConfig = Object.fromEntries(
-    Object.entries(config).map(([key, val]) => {
-      const shouldRedact = protectedProperties.includes(key) && !config.isLocalEnv
-      return [key, shouldRedact ? `[REDACTED:${String(val).length}]` : val]
-    })
-  )
+  if (!config.isLocalEnv) {
+    console.log('Server config', config)
+  } else {
+    const sanitisedConfig = Object.fromEntries(
+      Object.entries(config).map(([key, val]) => {
+        const shouldRedact = protectedProperties.includes(key)
+        return [key, shouldRedact ? `[REDACTED:${String(val).length}]` : val]
+      })
+    )
 
-  console.log('Server config', sanitisedConfig)
+    console.log('Server config', sanitisedConfig)
+  }
 }
