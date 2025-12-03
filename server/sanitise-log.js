77a51135649c6875023f836b4deff3769abf8b92
Matthew Quinton <30080427+MJQ7@users.noreply.github.com>
2025-12-03 15:03:11 +0000

    Sanitise AWS logging (#207)
    * Sanitise AWS logging
    
    https://eaflood.atlassian.net/browse/LTFRI-2204
    
    This ticket will stop the logging of senstive information in AWS logs
    
    * Sanitise sensitive configuration properties in server logs
    
    ADDED: introduced `protectedProperties` array to define sensitive keys
    UPDATED: modified logging to redact sensitive information in console output
    
    Changes:
        server/config.js updated
    
    * Split sanitising into own fuction and add env var to optionally enable full logging in debug
    
    ADDED: logSanitisedConfig function to redact sensitive data
    UPDATED: integrated logSanitisedConfig in config.js
    ADDED: IS_LOCAL_ENV flag to determine local environment
    
    Changes:
    	README.MD updated
    	server/config.js updated
    	server/sanitise-log.js created
    
    * Define the protected keys array away from the function to make it reusable across services.
    
    ADDED: protected properties array for sensitive keys
    UPDATED: logSanitisedConfig function to accept protected properties
    
    Changes:
    	.gitignore updated
    	server/config.js updated
    	server/sanitise-log.js updated
    
    * Updated package dependencies to their latest versions
    
    UPDATED: @arcgis/core to version 4.34.8
    UPDATED: @friendlycaptcha/sdk to version 0.1.33
    UPDATED: @hapi/hapi to version 21.4.4
    UPDATED: dotenv to version 17.2.3
    UPDATED: govuk-frontend to version 5.13.0
    UPDATED: joi to version 18.0.2
    UPDATED: jest to version 30.2.0
    UPDATED: lint-staged to version 16.2.7
    UPDATED: nodemon to version 3.1.11
    UPDATED: sass to version 1.94.2
    UPDATED: webpack to version 5.103.0
    
    Changes:
    	package.json updated
    
    * Improve readability and duplicate calls to config
    
    UPDATED: only check if the local env flag is set once
    
    Changes:
    	sanitise-log.js updated
    
    * Added unit tests for configuration and sanitisation log functionality.
    
    ADDED: unit tests for deriving environment flags in config
    ADDED: tests for logging behaviour based on environment in sanitise-log
    
    Changes:
        config.spec.js created
        sanitise-log.spec.js created
    
    * fix package lock refs
    
    * Reversed the logic for the redacted logging
    
    UPDATED: changed logging condition to display raw config when isLocalEnv is true
    UPDATED: adjusted tests to reflect new logging logic
    UPDATED: adjusted the test titles to be clearer
    ADDED: new test specifically for unset local environment flag
    
    Changes:
    	sanitise-log.js updated
    	sanitise-log.spec.js updated

---
+++ server/sanitise-log.js
@@ -0,0 +1,14 @@
+module.exports = function logSanitisedConfig (config, protectedProperties) {
+  if (config.isLocalEnv) {
+    console.log('Server config', config)
+  } else {
+    const sanitisedConfig = Object.fromEntries(
+      Object.entries(config).map(([key, val]) => {
+        const shouldRedact = protectedProperties.includes(key)
+        return [key, shouldRedact ? `[REDACTED:${String(val).length}]` : val]
+      })
+    )
+
+    console.log('Server config', sanitisedConfig)
+  }
+}
