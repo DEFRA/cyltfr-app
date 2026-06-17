d1302e450dd1079e671e3284d4cad111e3fdb556
JoshuaForrest <josh.forrest@aol.com>
2026-06-17 12:20:27 +0100

    Attempt to fix unit test github issue

--- server/public/static/js/friendly-captcha-handler.js
+++ server/public/static/js/friendly-captcha-handler.js
@@ -118,7 +118,10 @@ function onLoad () {
   }
 }
 
-onLoad()
+// Only auto-initialise in a real browser environment, not under Jest/Node
+if (typeof module === 'undefined') {
+  onLoad()
+}
 
 // Export for testing (UMD pattern)
 if (typeof module !== 'undefined' && module.exports) {
