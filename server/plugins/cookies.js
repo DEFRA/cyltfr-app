9e9d5815fab2aa147d5f7c102633bb76948e9d52
Matthew Collins <48916014+Matthew-Collins-Defra@users.noreply.github.com>
2026-09-03 15:03:15 +0100

    Revert "Merge pull request #261 from DEFRA/feature/ltfri-2544"
    This reverts commit f7542c1b1763f17f72368f0e5bc1e20ff73baa9b.

--- server/plugins/cookies.js
+++ server/plugins/cookies.js
@@ -1,8 +1,4 @@
 const config = require('../config')
-const {
-  getCookiePolicy,
-  removeAnalyticsCookies
-} = require('../services/cookie-policy')
 
 const cookiePolicyOptions = {
   ttl: 1000 * 60 * 60 * 24 * 365,
@@ -20,17 +16,14 @@ module.exports = {
 
       server.ext('onPreResponse', (request, h) => {
         if (request.response.variety === 'view') {
-          const cookiesPolicy = getCookiePolicy(request)
+          const { state } = request
+          const cookiesPolicy = (state || {}).cookies_policy
           const response = request.response
           const context = response.source.context || {}
 
           Object.assign(context, { cookiesPolicy })
 
           response.source.context = context
-
-          if (cookiesPolicy?.analytics === false) {
-            removeAnalyticsCookies(request, h)
-          }
         }
 
         return h.continue
